96a168175b7837c71419e85f498026ab60a2df07
senesh sirimanne <65598597+seneshsirimanne00@users.noreply.github.com>
2021-09-23 02:08:26 +0530

    initial commit

---
+++ src/TodoCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+const TodoCard = (props) => {
+  const { todo } = props;
+  const { id, completed, title } = todo;
+  let history = useHistory();
+
+  return (
+    <div
+      style={{
+        backgroundColor: "green",
+        color: "white",
+        margin: "10px",
+        padding: "15px",
+        borderRadius: "10px",
+        width: "150px"
+      }}
+      onClick={() => history.push(`/todo/${id}`)}
+    >
+      <h4> {title} </h4>
+      <h6> {`Completed: ${completed}`} </h6>
+    </div>
+  );
+};
+
+export default TodoCard;
